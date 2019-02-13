1c6638b5dd06db61679ff23944cde2f84479b9f5
SEBASTIAN NIEBLAS ZAZUETA <sebas_nit_040899@hotmail.com>
2019-02-12 19:46:28 -0700

    Proyecto Cubo
    Go to "Proyecto Cubo"

---
+++ proyecto cubo/Assets/Library/Engine/Pendulum.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulum : MonoBehaviour
+{
+    Quaternion _Start, _end;
+    [SerializeField, Range(0.0f, 360f)]
+    private float _angle = 90.0f;
+
+    [SerializeField, Range(0.0f, 5.0f)]
+    private float _speed = 2.0f;
+
+    [SerializeField, Range(0.0f, 10.0f)]
+    private float _startime = 0.0f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _Start = PendulumRotation(_angle);
+        _end = PendulumRotation(-_angle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        _startime += Time.deltaTime;
+        transform.rotation = Quaternion.Lerp(_Start, _end, (Mathf.Sin(_startime * _speed + Mathf.PI / 2) + 1.0f) / 2.0f);
+    }
+
+    void ResetTimer()
+    {
+        _startime = 0.0f;
+    }
+
+    Quaternion PendulumRotation(float angle)
+    {
+        var pendulumRotation = transform.rotation;
+        var angleZ = pendulumRotation.eulerAngles.z + angle;
+
+        if (angleZ > 180)
+            angleZ -= 360;
+        else if (angleZ < -180)
+            angleZ += 360;
+
+        pendulumRotation.eulerAngles = new Vector3(pendulumRotation.eulerAngles.x, pendulumRotation.y, angleZ);
+
+
+      return pendulumRotation;
+    }
+}
---
+++ proyecto cubo/Assets/Shaders/Rim_Spec.shader
@@ -0,0 +1,57 @@
+﻿Shader "Custom/Rim_Spec"
+{
+	Properties
+	{
+		_MainTex("MainTex", 2D) = "white" {}
+		_Albedo("Albedo", Color) = (1, 1, 1, 1)
+		_BumpTex("Normal", 2D) = "bump" {}
+		_NormalAmount("Normal Amount", Range(-3, 3)) = 1
+		_RimColor("Rim Color", Color) = (1, 1, 1, 1)
+		_RimPower("Rim Amount", Range(0.5, 8.0)) = 1
+		_Spec("Specular", Range(0, 1)) = 0.5
+		_Gloss("Gloss", Range(0, 1)) = 0.5
+		_SpecColor("SpecColor", Color) = (1, 1, 1, 1)
+	}
+
+	SubShader
+	{
+		CGPROGRAM
+
+		#pragma surface surf BlinnPhong
+
+		sampler2D _MainTex;
+		sampler2D _BumpTex;
+		half4 _Albedo;
+		float _NormalAmount;
+		float4 _RimColor;
+		float _RimPower;
+
+		half _Spec;
+		fixed _Gloss;
+
+		struct Input
+		{
+			float2 uv_MainTex;
+			float2 uv_BumpTex;
+			float3 viewDir;
+		};
+
+		void surf(Input IN, inout SurfaceOutput o)
+		{
+			o.Albedo = tex2D(_MainTex, IN.uv_MainTex).rgb * _Albedo.rbg;
+			float3 normal = UnpackNormal(tex2D(_BumpTex, IN.uv_BumpTex));
+			normal.z = normal.z / _NormalAmount;
+			o.Normal = normal;
+
+			//Rim
+			half rim = 1.0 - saturate(dot(normalize(IN.viewDir), o.Normal));
+			o.Emission = _RimColor.rgb * pow(rim, _RimPower);
+
+			o.Specular = _Spec;
+			o.Gloss = _Gloss;
+		}
+
+		ENDCG
+	}
+
+}
